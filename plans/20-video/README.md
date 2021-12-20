c8fb4ae1e3f280c4e5b7b48a5f1f462ee4024efa
azikreed <azikreed@gmail.com>
2021-12-20 15:02:34 +0500

    changed

---
+++ plans/20-video/README.md
@@ -0,0 +1,8 @@
+[20-video: MERN Stack Project Series | Ecommerce Website | User Login And Product Page - 31
+](https://youtu.be/3pylNewFvfM)
+
+**Branch name: `page`**
+
+> Ushbu videoda tushunganlarim va ketma-ketliklar:
+1. **controller** papkasi ichidagi **product.js** file da **getProductDetailsById** nomli function yaratdim.
+2. Va ushbu functionni **routes** papkasi ichidagi **product.js** file da GET methodi orqali chaqirib oldim.
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { requireSignin, adminMiddleware } = require('../common-middleware');
-const { createProduct, getProductsBySlug } = require('../controller/product');
+const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controller/product');
 const multer = require('multer');
 const router = express.Router();
 const shortid = require('shortid');
@@ -19,5 +19,6 @@ const upload = multer({ storage })
 
 router.post('/product/create', requireSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 router.get('/products/:slug', getProductsBySlug )
+router.get('/product/:productId', getProductDetailsById)
 
 module.exports = router;
