bab735143148b88afc745d33bcfc28291cfc2722
azikreed <azikreed@gmail.com>
2021-12-21 09:28:34 +0500

    changed

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { addItemToCart } = require('../controller/cart');
+const { addItemToCart, getCartItems } = require('../controller/cart');
 const { requireSignin, userMiddleware } = require('../common-middleware');
 const router = express.Router();
 
 router.post('/user/cart/addtocart', requireSignin, userMiddleware, addItemToCart);
+router.get('/user/getCartItems', requireSignin, userMiddleware, getCartItems)
 
 module.exports = router;
