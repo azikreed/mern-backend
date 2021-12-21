f0b70dd85f46da57f804349a3424d3b0cd20b96a
azikreed <azikreed@gmail.com>
2021-12-21 14:29:03 +0500

    added branch orders_page

--- src/controller/order.js
+++ src/controller/order.js
@@ -1,24 +1,31 @@
-const Order = require('../models/order');
+const Order = require("../models/order");
+const Cart = require("../models/cart");
 
 exports.addOrder = (req, res) => {
-    req.body.user = req.user._id;
-    const order = new Order(req.body);
-    order.save((error, order) => {
-        if(error) return res.status(400).json({error});
-        if(order) {
-            res.status(201).json({order});
+  Cart.deleteOne({ user: req.user._id })
+  .exec((error, result) => {
+    if (error) return res.status(400).json({ error });
+    if (result) {
+      req.body.user = req.user._id;
+      const order = new Order(req.body);
+      order.save((error, order) => {
+        if (error) return res.status(400).json({ error });
+        if (order) {
+          res.status(201).json({ order });
         }
-    })
-}
+      });
+    }
+  });
+};
 
 exports.getOrders = (req, res) => {
-    Order.find({user: req.user._id})
+  Order.find({ user: req.user._id })
     .select("_id paymentStatus items")
     .populate("items.productId", "_id name productPictures")
     .exec((error, orders) => {
-        if(error) return res.status(400).json({error});
-        if(orders) {
-            res.status(200).json({orders})
-        }
-    })
-}
+      if (error) return res.status(400).json({ error });
+      if (orders) {
+        res.status(200).json({ orders });
+      }
+    });
+};
